72948362b1e8a6ec17fd98ef215a2ce834e6cbe0
Francinette Dias Borges <francinetteborges@seap.ma.gov.br>
2020-07-29 23:53:26 -0300

    #5 - Criação das models

---
+++ implementacao/estoque/src/main/java/com/ithappens/estoque/model/Usuario.java
@@ -0,0 +1,37 @@
+package com.ithappens.estoque.model;
+
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "ES11_USUARIO")
+@SequenceGenerator(
+        name = "ES11_USUARIO_SEQ",
+        sequenceName = "ES11_USUARIO_SEQ",
+        allocationSize = 1
+)
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ES11_USUARIO_SEQ")
+    @Column(name = "ES11_COD_USUARIO")
+    private Long id;
+
+    @NotBlank(message = "O nome do usuário deve ser informado")
+    @Column(name = "ES11_NOME")
+    private String nome;
+
+    @CPF(message = "Informe um CPF válido")
+    @NotBlank(message = "O CPF do usuário deve ser informado")
+    @Column(name="ES11_CPF")
+    private String cpf;
+
+    @NotBlank(message = "A senha do usuário deve ser informada")
+    @Column(name = "ES11_SENHA")
+    private String senha;
+
+
+}
